4f985499ad949ac49ab95e4a6b105502bb0a2973
hahoetal <49137087+hahoetal@users.noreply.github.com>
2020-02-09 21:15:47 +0900

    도형 그리기
    for 반복문, if 조건문을 활용해서 도형 그리기

---
+++ figure.py
@@ -0,0 +1,64 @@
+num = int(input('세로 길이 : '))
+print()
+# 사각형
+for i in range(num):
+    for j in range(num):
+        print('*', end = '')
+    print()
+    
+print()
+
+# 직각 삼각형
+for i in range(num):
+    for j in range(num):
+        if j <= i:
+            print('*', end = '')
+        else:
+            print(' ', end = '')
+    print()
+
+print()
+
+# 뒤집어진 직각 삼각형
+for i in range(num):
+    for j in range(num):
+        if i <= j:
+            print('*', end = '')
+        else:
+            print(' ', end = '')
+    print()
+
+print()
+
+#for i in range(num):
+#    for j in range(num):
+#        if j < i:
+#            print(' ', end = '')
+#        else:
+#            print('*', end = '')
+#    print()
+
+# 사다리꼴
+
+for i in range(num):
+    for j in range(2 * num - 1):
+        if j < (num - 1) - i:
+            print(' ', end = '')
+        else:
+            print('*', end = '')
+    print()
+    
+print()
+
+# 이등변 삼각형
+
+for i in range(num):
+    for j in range(2 * num - 1):
+        if j < (num - 1) - i:
+            print(' ', end = '')
+        elif j >= (num + i):
+            print(' ', end = '')
+        else:
+            print('*', end = '')
+    print()
+print()
